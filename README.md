0a030385d3ffac145e755c287f713d023581ddfd
Rustam Zagirov <rustam@zagirov.name>
2013-10-30 03:28:00 +0400

    add script, change icons

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 Этот скрипт заменяет иконки серпа и молота на нормальные флаги
 
-Использованы иконки:
+Использованы иконки с сайтов:
 
-* http://www.iconattitude.com/icons/ico/5474/flag-ru.html
-* http://www.iconattitude.com/icons/ico/5566/flag-us.html
-License: Free for personnal use
+* http://www.iconattitude.com
+* http://www.iconfinder.com
---
+++ change.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  directory="$HOME/Library/Keyboard Layouts/Ilya Birman Typography Layout.bundle/Contents/Resources"
+  if [ -d "$directory" ]; then
+		us="$directory/English - Ilya Birman Typography.icns"
+		ru="$directory/Russian - Ilya Birman Typography.icns"
+		mv "$us"{,.bak}
+		mv "$ru"{,.bak}
+		echo 'Downloading icons'
+		curl -sSL https://github.com/stamm/birman_layout_normal_icons/raw/master/flag-ru.icns > "$ru"
+		curl -sSL https://github.com/stamm/birman_layout_normal_icons/raw/master/flag-us.icns > "$us"
+		echo 'done'
+		echo 'You need to restart Mac to apply icons'
+	else
+		echo 'You not install Birman Typography layout'
+	fi
+else
+  echo 'You use not mac'
+fi
